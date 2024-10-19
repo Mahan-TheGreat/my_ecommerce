f5c0c469c0942bb06f896b73372bc3725710dde5
mahan <megamindmaahan@gmail.com>
2024-10-18 22:08:49 -0400

    Updated to include variables from .env file

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
 from dotenv import load_dotenv
-
+import os
 load_dotenv()
 app = Flask(__name__)
 
 # MongoDB connection
-app.config.from_prefixed_env('MYAPP')
-client = MongoClient(app.config['CONNECTION_STRING'])
+MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
+
+client = MongoClient(f'mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@devops.nxb7m.mongodb.net/?retryWrites=true&w=majority&appName=DevOps')
 db = client.shop_db
 products_collection = db.products
 
